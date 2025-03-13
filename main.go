753aa7e0b97fe8899aa9ffae7cdaafcad2e076ca
Eduardo Morales <emoral435@gmail.com>
2025-03-13 11:10:30 -0500

    feat: initialized Go Firebase SDK
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+
+	firebase "firebase.google.com/go"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("| Running Frontend on http://localhost:3000 |")
+
+	// get our context for our app
+	app, err := firebase.NewApp(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	if err == nil {
+		fmt.Println("%w", app)
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("../frontend/dist/")))
 	http.ListenAndServe(":3000", nil)
 }
