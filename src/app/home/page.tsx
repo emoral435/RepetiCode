5af066b201d89a362e9962d9976b3135000cae3e
Eduardo Morales <emoral435@gmail.com>
2025-03-12 19:49:00 -0500

    ChP: considering switching lol
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,17 @@
+'use client'
+
+import { useEffect } from "react"
+import { auth } from "../lib/firebase-config";
+import { redirect } from "next/navigation";
+
 const Home = () => {
+	useEffect(() => {
+		if (!auth.currentUser) {
+      redirect('/login'); // if user is not logged in, redirect to login page immediately
+    }
+
+		console.log(`User: ${auth.currentUser}`)
+	}, []); // only check if the user is logged in once
 	return (
 		<div>
 			Home
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,7 @@ import { AuthTypes, AuthProviderEmails } from '../definitions/auth';
 import { loginWithGoogle, loginWithGithub } from '../lib/auth';
 import { redirect } from "next/navigation";
 import { auth } from '../lib/firebase-config';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 
@@ -27,24 +28,49 @@ const Login = () => {
       } else {
         await loginWithGithub();
       }
-
-      redirect('/home');
     } catch (error: any) {
-      console.error('Error signing in with popup', error);
+      console.error('Error signing in with popup', error.code);
       // user has used this email before with a different provider - merge the two providers together to sync their data
-      if (error.code.includes("auth/account-exists-with-different-credential")) {
-          let provider = error.customerData.verifiedProvider[0]
-          if (provider === AuthProviderEmails.GOOGLE) {}
-        }
-      } finally {
-        if (loggedInFlag) {
-          redirect('/home');
+      if (error.code = "auth/account-exists-with-different-credential") {
+        let provider = error.customData["verifiedProvider"][0]
+        console.log(`Provider: ${JSON.stringify(error.customData)}`)
+        // TODO also check if the user uses emal/password auth - to link, see: https://stackoverflow.com/a/76092918
+        if (provider === AuthProviderEmails.GOOGLE) {
+          loggedInFlag = false;
+          toast(`You tried logging in with a google account that is associated to a register ${AuthProviderEmails.GITHUB} account! Try logging in with your ${AuthProviderEmails.GOOGLE} account instead.`, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
+        } else {
+          loggedInFlag = false;
+          toast(`You tried logging in with a google account that is associated to a register ${AuthProviderEmails.GOOGLE} account! Try logging in with your ${AuthProviderEmails.GITHUB} account instead.`, {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            });
         }
       }
+    } finally {
+      if (loggedInFlag) {
+        redirect('/home');
+      }
+    }
   }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
+      <ToastContainer />
       <div className="max-w-md w-full text-center p-6">
         <h1 className="text-3xl font-bold text-blue-400 mb-6">Login</h1>
         <EmailForm />
